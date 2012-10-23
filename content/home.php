ad746ca7cf78387906c736be43c68427511f8cae
Matthew Ripari <mlripper33@gmail.com>
2012-10-23 19:04:13 -0400

    Mission statement
    added flag/ kenya project and mission statement

--- content/home.php
+++ content/home.php
@@ -4,4 +4,35 @@
 ?>
 <body>
 	
+<div id="container">
+
+<div id="header" style="text-align:center">
+	<span>
+		<img src="http://www.viesafari.com/images/stories/Kenya-flag.gif" align="middle" width="150" height="100"></img>
+		<i  style="vertical-align:text-top">Kenya Project</i></br>
+		<i>Are you interested in social entrepreneurship? </br>
+		   Are you interested in learning about other cultures or willing to challenge yourself expand your worldview? </br>
+		   Do you wish you had the opportunity to create meaningful change even as a student? </br>
+		   Do you want the chance to travel to Kenya this summer and make tangible impact in
+		   partnership with local Kenyans to improve the economy and standard of living there? </br>
+		</i>
+	</span>
+</div>
+
+<div id="menu" style="background-color:#FFD700;height:200px;width:100px;float:left;">
+<b>Menu</b><br>
+HTML<br>
+CSS<br>
+JavaScript</div>
+
+<div id="content" style="background-color:#EEEEEE;height:200px;width:400px;float:left;">
+Content goes here</div>
+
+<div id="footer" style="background-color:#FFA500;clear:both;text-align:center;"></div>
+
+</div>
+
+
+
+	
 </body>

94a527cde0e4c79aee036716b3002843990b2822
Jen <jeneisenberg5@gmail.com>
2012-10-28 19:01:48 -0400

    Centered Mission Statement

---
+++ content/content.home.php
@@ -0,0 +1,25 @@
+ <body style="background-color:#F5F5DC">
+ 	
+<div id="container" style="width:1000px; margin: 0 auto">
+
+	<div id="header" style="text-align:center">
+		<span>
+			<p style="border:3px solid #000000;"></br>
+			<img src="http://www.viesafari.com/images/stories/Kenya-flag.gif" align="middle" width="150" height="100"></img>
+			<i  style="vertical-align:text-top">Kenya Project</i></br> 
+			<i>Are you interested in social entrepreneurship? </br>
+			   Are you interested in learning about other cultures or willing to challenge yourself expand your worldview? </br>
+			   Do you wish you had the opportunity to create meaningful change even as a student? </br>
+			   Do you want the chance to travel to Kenya this summer and make tangible impact in
+			   partnership with local Kenyans to improve the economy and standard of living there? </br>
+			</i></br>
+			</p>
+		</span>
+	
+	</div>
+
+</div>
+
+
+	
+ </body>

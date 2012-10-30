9d76a8a29be58444e8548590dbc5a5e5afde3686
Jen <jeneisenberg5@gmail.com>
2012-10-30 19:46:44 -0400

    finished content.home.php, aboutus.php, apply.php

---
+++ content/aboutus.php
@@ -0,0 +1,61 @@
+ <body style="background-color:#F5F5DC">
+ 	<div id="container" style="width:1000px">
+ 		<div id="text">
+	 		<b> Who do we want on our team?</b>
+	 			<p>
+					<br> People who are ambitious, creative, curious, passionate, daring, team players, leaders, and those with the skills needed to address the problems found in Kithoka, Kenya.  We want to put together a team of students studying many different disciplines; so, all majors are encouraged to apply.  No previous entrepreneurship experience is necessary, but be aware that we are looking for committed and dedicated people. 
+					The internship is open to all undergraduates.
+				</p>
+					 
+
+			<b>Contact mpowered-kenya@umich.edu if you have any questions. </b><br>
+
+			<br>
+			<b> What will you be doing?</b>
+				<p>
+			<br>The Kenya Team will participate in a crash course for social entrepreneurship organized by MPowered along with cultural exposure to Swahili and what life is like in Kenya throughout the winter semester.
+			During 3 weeks of actual experience in Kenya in June, you will go through idea generation, market research, pivoting, prototyping, and business model/pitch generation in partnership with young adults from the Kithoka and surrounding community.
+			The trip will conclude with a presentation/pitch to influential local leaders in the community to receive feedback on the business model you developed with Kenyans and determine future viability and funding.
+			After your return to campus in the fall, you will re-pitch the idea to a panel of UM professors, entrepreneurs, and venture capitalists to receive feedback.  
+			From there, you can decide to take the initiative to continue working on the project with Kenyans and apply to Techarb or enroll in a social entrepreneurship practicum course.  Or, you can pass off the idea to the MichiKen incubator where a local Kenyan will take over operations and build the idea into a business that can improve the economy in Kithoka, Kenya.
+				</p>
+
+
+			<b> What is it like in Kithoka, Kenya?</b>
+	<p> <br> Pictures from last year's trip. </p>
+
+	<b>Lodging</b>
+	<p>
+		<br>You will be staying at the Thiiri Center, a community-cultural center with modern amenities.  This center provides lodging at a much higher standard of living than most Kenyan homes, but it will be different from America.  
+	</p>
+	<b>Immunizations</b>
+	<p>
+		<br>The Center for Disease Control website contains important information for traveling to Kenya.  The CDC recommends visiting a travel clinic four to six weeks in advance of your trip.  The University Health Service (UHS) travel clinic is a very simple and convenient option.  Malaria risk is present in Kithoka, Kenya
+		Clinics and full service hospitals are located within a one hour drive of our project locations and our hosts at the Thiiri Center are familiar with the local medical resources.
+	</p>
+	<b>Costs</b>
+	<p>
+	<br>Costs will depend on the amount we fundraise.  You will be participating in fundraising with MPowered as well.  Last year, MPowered covered all costs of living and staying in Kenya, while trip participants paid for their plane ticket, personal immunizations, and visas. 
+	If you have financial concerns about your ability to participate, don't hesitate to reach out to us at mpowered-kenya@umich.edu, and we will do our best.
+	</p>
+	<br><b>Commonly Asked Questions:</b><br>
+	
+	<br><b> What if I'm not passionate about business?</b>
+		<p>
+			<br>We use the term business very broadly.  This does not mean you need business interest or knowledge.  In fact, the most productive teams have people from all different backgrounds like business, engineering, art & design, etc.  By business, we mean your goal is to generate a sustainable model for creating economic value in a for-profit or not-for-profit way.  The primary goal is to create social value in job creation or standard of living.  You are encouraged to follow your passions and work on a project to make it sustainable.  For example, if you are passionate about water quality, your "business" can center on that.
+		</p>
+	<br><b>Do I work on my own idea or do we have one idea as a team?</b>
+		<p>
+		<br> When you arrive in Kenya, you'll be grouped with Kenyan locals who know the economy, environment, and have ideas of their own.  The project is a collaboration between us and them.  They will bring invaluable knowledge to the discussion.  There is no requirement for how many people you can work with.  You can work on your idea with Kenyans, you can work with another UM student and Kenyans, or all 7 UM students can work together with Kenyans on one idea.  What tends to happen is people have different interests and passions, so teams will split along those lines and you can focus on an idea you care about.
+		</p>
+	<br><b>What ideas did last year's team come up with?</b>
+		<p>
+		<br> Last year, we had six business plans produced from the 5 UM students who went and their partnership with Kenyan locals.  They were focused on eco-tourism, online Swahili tutoring, agriculture, an after-school program for youth, creation of soap for sanitation, and a job recruitment agency for high school graduates.  
+		</p>
+	<br><b>Will I really be able to start an actual business?</b>
+		<p>
+		<br> It's all about internal motivation and what you put into it.  Realistically, we can't promise that you'll find that amazing idea and become really successful with it.  It's important to recognize that what we are trying to do is difficult and the success rate is low.  What we can promise is an experience you'll never forget, a chance to apply knowledge and experience entrepreneurship unlike any other, and the reality of seeing your work change lives.  For the continuation of the business, it's up to how much time you invest in it.  If you decide to pursue it, we will be behind you 100%.  MPowered is here to provide as much support and resources as possible, but we recognize that you are a student and have those obligations as well.
+		</p>
+	</div>
+</div>
+</body>

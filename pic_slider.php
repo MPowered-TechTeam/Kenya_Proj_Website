419af9966e5014fd405e903dcde14569c5d76a38
Jeff Sallans <jeffsallans@gmail.com>
2012-10-30 19:48:09 -0400

    slide show working

---
+++ pic_slider.php
@@ -0,0 +1,33 @@
+<?php
+
+?>
+
+<style type="text/css">
+
+    #galleria{ width: 700px; height: 400px; background: #000 }
+</style>
+
+<head>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.js"></script>
+    <script src="galleria/galleria-1.2.8.min.js"></script>
+</head>
+<body>
+    <div id="galleria" class="galleria">
+
+<?php
+
+	for($i=1; $i < 53; $i++)
+	{
+        echo "<img src='images/img (".$i.").jpg'>";
+    }
+?>
+    </div>
+    <script>
+        Galleria.loadTheme('galleria/themes/classic/galleria.classic.min.js');
+        Galleria.run('#galleria');
+        $('#galleria').data('galleria').play();
+        $('.galleria').click(function() {
+			$('#galleria').data('galleria').play();
+        });
+    </script>
+</body>

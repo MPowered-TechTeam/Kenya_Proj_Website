c8a091077be7f2318ef158bdce3f98921362d51d
Jeff Sallans <jeffsallans@gmail.com>
2012-11-05 01:35:07 -0500

    another change

---
+++ side.php
@@ -0,0 +1,20 @@
+<?php
+
+?>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+<br />
+<br />
+<span class="fb-like" data-href="http://www.facebook.com/pages/MPowered-Entrepreneurship/263938600375830?fref=ts" data-width="200" data-font="arial"></span>
+<br />
+<br />
+<a href="https://twitter.com/MPowered" class="twitter-follow-button" data-show-count="false" data-size="large" >Follow @MPowered</a>
+			<script src="http://platform.twitter.com/widgets.js"
+			type="text/javascript"></script>

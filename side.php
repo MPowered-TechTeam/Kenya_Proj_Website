b70398d270fae7145cac57c37801b9b64c7fdae7
Praful Mathur <prafulm@umich.edu>
2012-11-06 16:36:51 -0500

    Changed like box to feed, does it look good?

--- side.php
+++ side.php
@@ -6,13 +6,13 @@
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
   js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=252873378088181";
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));</script>
 
 <br />
 <br />
-<span class="fb-like" data-href="http://www.facebook.com/MKenyaProject" data-width="200" data-font="arial"></span>
+<span class="fb-like-box" data-href="http://www.facebook.com/MKenyaProject" data-width="230" data-show-faces="false" data-stream="true" data-header="false"></span>
 <br />
 <br />
 <a href="https://twitter.com/MPowered" class="twitter-follow-button" data-show-count="false" data-size="large" >Follow @MPowered</a>
